3f875c55e25943bc3b386f5c418d06cc0f790d93
Glen <glen@suterusu.io>
2019-12-22 22:16:32 +0800

    Update configure.sh
    Signed-off-by: Glen <glen@suterusu.io>

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 opam install ocamlbuild
-opam install ocamlfind
-opam install digestif websocket lwt lwt_log conduit-lwt-unix cohttp-lwt-unix lwt_ppx hex
+opam install ocamlfind camlp5
+opam install digestif websocket lwt lwt_log conduit-lwt-unix cohttp-lwt-unix lwt_ppx hex yojson
 
