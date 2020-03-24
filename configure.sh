7b19afe179d6df5a6ccead4ba9c6e3189ece9456
YI <uuuuuu@protonmail.com>
2020-03-24 11:57:34 +0800

    update building instructions

--- configure.sh
+++
@@ -1,2 +0,0 @@
-opam pin add tweetnacl https://github.com/vbmithr/ocaml-tweetnacl.git
-opam install ocamlbuild ocamlfind camlp5 yojson stdint digestif websocket lwt lwt_log conduit-lwt-unix cohttp-lwt-unix lwt_ppx hex xxhash tweetnacl
